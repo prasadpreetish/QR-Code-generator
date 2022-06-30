74dbfd31a0603c3bcae12559f95997766f85ea3f
Preetish Rupa Prasad <www.preetishpd@gmail.com>
2022-06-30 12:37:11 +0530

    generated image from the above qr code which can be downloaded

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import QRCode from "react-qr-code";
+import domtoimage from 'dom-to-image';
 import { useState } from 'react';
 
 const App = () => {
@@ -8,10 +9,30 @@ const App = () => {
   const myFunc = (e) => {
     setVal(e.target.value)
   }
+
+  function saving(){
+    
+    var node = document.getElementById('qrcodeforimg');
+
+    domtoimage.toPng(node)
+        .then(function (dataUrl) {
+            var img = new Image();
+            img.src = dataUrl;
+            document.body.appendChild(img);
+        })
+        .catch(function (error) {
+            console.error('oops, something went wrong!', error);
+        });  
+  }
+
   return (
     <>
       <div className="app--container"><input type="text" onChange={myFunc} className="input--field--for--qrcode" />
-        <QRCode value={val} className="qrcodemadewithdivs" /></div>
+        <div id="qrcodeforimg">
+          <QRCode value={val} className="qrcodemadewithdivs" />
+        </div>
+        <button onClick={saving}>Capture</button>
+      </div>
     </>
   )
 }
